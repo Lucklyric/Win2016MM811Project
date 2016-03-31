b96f8b5efd58d330272ff315cd8af8b57dd89dbb
hongzu <li.zachary@163.com>
2016-03-30 21:59:33 -0600

    conflict

--- serverscript.py
+++ serverscript.py
@@ -2,10 +2,9 @@ import urllib2
 import json
 from pprint import pprint
 import mysql.connector
+
 #Neighbourhood Boundaries
 urlNB = "https://data.edmonton.ca/api/views/nckr-nnqj/rows.json?accessType=DOWNLOAD"
-#Edmonton Catholic Schools (2015 - 2016)
-urlECS="https://data.edmonton.ca/api/views/f6w2-hzex/rows.json?accessType=DOWNLOAD"
 #Neighbourhoods (Centroid Point)
 urlNC="https://data.edmonton.ca/api/views/3b6m-fezs/rows.json?accessType=DOWNLOAD"
 #Playgrounds
@@ -14,14 +13,45 @@ urlP="https://data.edmonton.ca/api/views/c4nr-3quz/rows.json?accessType=DOWNLOAD
 urlTC="https://data.edmonton.ca/api/views/25b5-e682/rows.json?accessType=DOWNLOAD"
 #Edmonton Public Schools (2015 - 2016)
 urlEPS="https://data.edmonton.ca/api/views/ehbr-emhe/rows.json?accessType=DOWNLOAD"
+#Edmonton Catholic Schools (2015 - 2016)
+urlECS="https://data.edmonton.ca/api/views/f6w2-hzex/rows.json?accessType=DOWNLOAD"
 #Dwelling Unit By Structure Type (Neighbourhood) 
 urlDUST="https://data.edmonton.ca/api/views/bjgi-976r/rows.json?accessType=DOWNLOAD"
 #Trees 
 urlT="https://data.edmonton.ca/api/views/eecg-fc54/rows.json?accessType=DOWNLOAD"
 
-#read url
+#read all urls
 jfile = urllib2.urlopen(urlNB).read()
 data = json.loads(jfile)
-#for i in range len(data)
-pprint(data["meta"]["view"]["name"])
+pprint(data["meta"]["view"]["columns"][9]["name"])
+pprint(len(data["data"]))
+
+
+#jfile = urllib2.urlopen(urlNC).read()
+#data = json.loads(jfile)
+#pprint(data["meta"]["view"]["name"])
+
+#jfile = urllib2.urlopen(urlP).read()
+#data = json.loads(jfile)
+#pprint(data["meta"]["view"]["name"])
+
+#jfile = urllib2.urlopen(urlTC).read()
+#data = json.loads(jfile)
+#pprint(data["meta"]["view"]["name"])
+
+#jfile = urllib2.urlopen(urlEPS).read()
+#data = json.loads(jfile)
+#pprint(data["meta"]["view"]["name"])
+
+#jfile = urllib2.urlopen(urlECS).read()
+#data = json.loads(jfile)
+#pprint(data["meta"]["view"]["name"])
+
+#jfile = urllib2.urlopen(urlDUST).read()
+#data = json.loads(jfile)
+#pprint(data["meta"]["view"]["name"])
+
+#jfile = urllib2.urlopen(urlT).read()
+#data = json.loads(jfile)
+#pprint(data["meta"]["view"]["name"])
 
