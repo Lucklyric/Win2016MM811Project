9c276ce0c1dc34425a3aba66e15a4607c5b64cc3
Hongzu Li <hongzuli@Hongzus-Mac-mini.local>
2016-03-31 01:46:16 -0600

    get neighbourhood and put in the table

--- serverscript.py
+++ serverscript.py
@@ -3,6 +3,13 @@ import json
 from pprint import pprint
 import mysql.connector
 
+cnx = mysql.connector.connect(user='root', password='',
+                              host='localhost',
+                              database='mm811project')
+cursor = cnx.cursor()
+
+#Neighbourhood name
+urlNN = "https://data.edmonton.ca/api/views/65fr-66s6/rows.json?accessType=DOWNLOAD"
 #Neighbourhood Boundaries
 urlNB = "https://data.edmonton.ca/api/views/nckr-nnqj/rows.json?accessType=DOWNLOAD"
 #Neighbourhoods (Centroid Point)
@@ -21,14 +28,42 @@ urlDUST="https://data.edmonton.ca/api/views/bjgi-976r/rows.json?accessType=DOWNL
 urlT="https://data.edmonton.ca/api/views/eecg-fc54/rows.json?accessType=DOWNLOAD"
 
 #read all urls
+jfile = urllib2.urlopen(urlNN).read()
+dataNN = json.loads(jfile)
 jfile = urllib2.urlopen(urlNB).read()
-data = json.loads(jfile)
-pprint(data["meta"]["view"]["columns"][9]["name"])
-pprint(len(data["data"]))
+dataNB = json.loads(jfile)
+jfile = urllib2.urlopen(urlNC).read()
+dataNC = json.loads(jfile)
+
+add_neighbourhood = ("INSERT INTO neighbourhood_tmp "
+               "(NEIGHBOURHOOD_NAME,NEIGHBOURHOOD_AREA,NEIGHBOURHOOD_LATI,NEIGHBOURHOOD_LONG) "
+               "VALUES (%s,%s,%s,%s)")
+for i in range(len(dataNC["meta"]["view"]["columns"])):
+    pprint(dataNC["meta"]["view"]["columns"][i]["name"])
+area=""
+latitude=0
+longitude=0
+for i in range(len(dataNN["data"])):
+    for j in range(len(dataNB["data"])):
+        if dataNB["data"][i][8] == dataNN["data"][i][8]:
+            area = dataNB["data"][i][9]
+            break
+        
+    for j in range(len(dataNC["data"])):
+        if dataNC["data"][i][9] == dataNN["data"][i][8]:
+            latitude = dataNC["data"][i][10]
+            longitude = dataNC["data"][i][11]
+            #pprint(latitude);
+            break    
+    neighbourdata=(dataNN["data"][i][8],area,latitude,longitude)
+    cursor.execute(add_neighbourhood, neighbourdata) 
+    cnx.commit()
+    
+update_area = ("UPDATE neighbourhood_tmp"
+            "SET NEIGHBOURHOOD_AREA=%s WHERE NEIGHBOURHOOD_NAME=%s")
+    
 
 
-#jfile = urllib2.urlopen(urlNC).read()
-#data = json.loads(jfile)
 #pprint(data["meta"]["view"]["name"])
 
 #jfile = urllib2.urlopen(urlP).read()
@@ -55,3 +90,5 @@ pprint(len(data["data"]))
 #data = json.loads(jfile)
 #pprint(data["meta"]["view"]["name"])
 
+cursor.close()
+cnx.close()
