28f5f297b47e13aee1537f98b1d105185916ec66
Lucklyric <xinyao1@ualberta.ca>
2016-03-31 01:58:55 -0600

    1. First page

--- cordova_project/live-in-edmton/www/js/google_map.js
+++ cordova_project/live-in-edmton/www/js/google_map.js
@@ -4,7 +4,7 @@
 function initialize() {
     var mapProp = {
         center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:12,
+        zoom:10,
         mapTypeId:google.maps.MapTypeId.ROADMAP
     };
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
