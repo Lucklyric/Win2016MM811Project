d1cac54daf6026942b4368aa5e470eaee56853e5
Hongzu Li <hongzuli@Hongzus-Mac-mini.local>
2016-03-31 22:34:56 -0600

    output.json

--- python_script/serverscript.py
+++ python_script/serverscript.py
@@ -25,33 +25,34 @@ def convert_to_list(polygon):
     coords_list=[]
     for i in range(len(coords)):
         plist=[]
-        for j in coords.split():
+        coord=coords[i]
+        for j in coord.split():
             plist.append(j.strip())
         coords_list.append(plist)
-    pprint(coords_list)
+    #pprint(coords_list)
     return coords_list
 
 def json_output():
-    #with open(output, 'wb') as outfile:
+    with open('output.json', 'wb') as outfile:
         cursor.execute("SELECT * FROM major_dataset")
         data = cursor.fetchall()
         for row in data:
             pprint(row[1])
             cursorn=cnx.cursor()
-            cursorn.execute("SELECT * FROM neighbourhood_tmp WHERE NEIGHBOURHOOD_NAME='%s'",row[1])
+            cursorn.execute("SELECT * FROM neighbourhood_tmp WHERE NEIGHBOURHOOD_NAME='?'",row[1])
             ndata = cursorn.fetchall()
             pprint(cursorn.rowcount)
             area=[]
             centroid=[]       
             for r in ndata:
-                pprint(r[0])
-                area=conver_to_list(r[0])
-                latitude=r[1]
-                longitude=r[2]
-            #sequences=[row[2],row[3],row[4],row[5],row[6],row[7],row[9],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],area,latitude,longitude]
-            #neibourhood={}        
-            #neibourhood[row[1]]=sequence
-            ##json.dump(neibourhood, outfile)   
+                #pprint(r[1])
+                area=convert_to_list(r[1])
+                latitude=r[2]
+                longitude=r[3]
+            sequence=[row[2],row[3],row[4],row[5],row[6],row[7],row[9],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],area,latitude,longitude]
+            neibourhood={}        
+            neibourhood[row[1]]=sequence
+            json.dump(neibourhood, outfile)   
             #pprint(neighbourhood)
             cursorn.close()
 
