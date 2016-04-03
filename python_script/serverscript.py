4cbf2d779e2896fc4db59bcfffd06cc8b68a1ff0
Hongzu Li <hongzu@ualberta.ca>
2016-04-03 14:55:18 -0600

    insert user query

--- python_script/serverscript.py
+++ python_script/serverscript.py
@@ -80,7 +80,7 @@ def json_output(user_query):
     cursor.close()       
     rank = sorted(out_array, key=itemgetter('score'), reverse=True)   
     #with open('output.json', 'wb') as outfile:
-    print json.dumps(rank)   
+    #print json.dumps(rank)   
 
 def ioio():
     with open('output.json') as data_file:    
@@ -105,15 +105,24 @@ def calculate_score(attributes,user_query,maxs):
             y.append(user_query[i])
     return euclidean_distance(x,y)
    
-def main(argv):
+def insert_user_query(user_query):
+    cursoru = cnx.cursor()
+    insert_query=("INSERT INTO user_query  (NUM_PLAYGROUNDS,NUM_PUBLIC_SCHOOLS,NUM_CATHOLIC_SCHOOLS,NUM_SINGLE,NUM_DUPLEX,NUM_ROW_HOUSE,NUM_APARTMENT_FIVE,NUM_APARTMENT_FOUR,NUM_HOTEL,NUM_AGE_FOURTEEN,NUM_AGE_THIRTYFIVE,NUM_AGE_SIXTY,NUM_AGE_SIXTYPLUS,NUM_EMPLOYMENT_STUDENT,NUM_EMPLOYMENT_UNEMPLOYED,NUM_EMPLOYMENT_EMPLOYED) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+    query_data=(user_query[0],user_query[1],user_query[2],user_query[3],user_query[4],user_query[5],user_query[6],user_query[7],user_query[8],user_query[9],user_query[10],user_query[11],user_query[12],user_query[13],user_query[14],user_query[15])
+    cursoru.execute(insert_query,query_data)
+    cnx.commit()
+    cursoru.close()
+
+def main():
     user_query=[]
     for i in argv.split(","):
         user_query.append(i)
     #user_query=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    insert_user_query(user_query)    
     json_output(user_query)
     #ioio()
     cnx.close()       
     
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
     
