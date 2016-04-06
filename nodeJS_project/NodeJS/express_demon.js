744910f76d2912c7f6dd289af9492d6b390515cc
Lucklyric <xinyao1@ualberta.ca>
2016-04-05 18:45:52 -0600

    1. Update the service management page 2. Add more nodejs api

--- nodeJS_project/NodeJS/express_demon.js
+++ nodeJS_project/NodeJS/express_demon.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pythonShell = require('python-shell');
-var service_status = 1;
+var service_status = "1";
 var options = {
 	mode:'text',
   	pythonPath: "E:/Program Files/Python27/python.exe"
@@ -11,6 +11,30 @@ var options = {
 
 var app = express();
 var count = 0;
+
+
+function updateTable(res){
+	pythonShell.run('../../python_script/update_server.py', options, function (err, results) {
+  	if (err) throw err;
+  	// results is an array consisting of messages collected during execution
+  	    if (res){
+  			res.send("1");
+  		}
+  		console.log('end: requet updateTable');
+	});
+}
+
+function updateQuery(res){
+ 	pythonShell.run('../../python_script/update_query.py', options, function (err, results) {
+  	if (err) throw err;
+  	// results is an array consisting of messages collected during execution
+  		if (res){
+  			res.send("1");
+  		}
+  		console.log('end: requet querytable');
+	});
+}
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -65,8 +89,6 @@ app.post('/fakequery',function (req, res) {
 	// worker.send('a');
 	console.log('start:query');
 	options["args"] = [req.body["string"]];
-
-	console.log("run python shell"+ options);
     pythonShell.run('../../python_script/serverscript.py', options, function (err, results) {
   	if (err) throw err;
   	// results is an array consisting of messages collected during execution
@@ -83,41 +105,42 @@ app.post('/querycount',function (req, res) {
 	// 	res.send(m);
 	// });
 	// worker.send('a');
-	
+	console.log('request query count');
     pythonShell.run('../../python_script/get_query_count.py', options, function (err, results) {
   	if (err) throw err;
   	// results is an array consisting of messages collected during execution
   		res.send(results);
-  		console.log('end:query');
+  		console.log('end:request query count');
 	});
 });
 
 app.post('/service_status',function (req, res) {
-
+	console.log('request service_status');
 	res.send(service_status);
-    
+	console.log('end:service_status');
 });
 
 
 
-app.post('/tableInformation',function (req, res) {
-
-    pythonShell.run('../../python_script/get_query_count.py', options, function (err, results) {
+app.post('/table_information',function (req, res) {
+	console.log('request tableInformation');
+    pythonShell.run('../../python_script/database_info.py', options, function (err, results) {
   	if (err) throw err;
   	// results is an array consisting of messages collected during execution
   		res.send(results);
-  		console.log('end:query');
+  		console.log('end:rquent tableInformation');
 	});
+
 });
 
-app.post('/updateTable',function (req, res) {
-	
-    pythonShell.run('../../python_script/update_server.py', options, function (err, results) {
-  	if (err) throw err;
-  	// results is an array consisting of messages collected during execution
-  		res.send(results);
-  		console.log('end:query');
-	});
+app.post('/update_table',function (req, res) {
+	console.log('request updateTable');
+    updateTable(res);
+});
+
+app.post('/update_query',function (req, res) {
+	console.log('request querytable');
+   	updateQuery(res);
 });
 
 
@@ -125,11 +148,17 @@ app.post('/updateTable',function (req, res) {
 * Here are some start shell command
 */
 app.post('/restart_web_service',function (req, res) {
+	console.log('request restart_web_service');
 	res.send("0");
+	console.log('end:restart_web_service');
+
 });
 
 app.post('/stop_web_service',function (req, res) {
+	console.log('request stop_web_service');
 	res.send("0");
+	console.log('end:stop_web_service');
+
 });
 
 
